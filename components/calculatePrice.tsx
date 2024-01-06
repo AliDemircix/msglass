aba4fd4b85902d9b5837cb6d1351e22514ca1601
ENSAR DOGAN <doganermansur@icloud.com>
2024-01-06 21:15:27 +0100

    refactor navbar and calculatePrice

--- components/calculatePrice.tsx
+++ components/calculatePrice.tsx
@@ -16,63 +16,69 @@ export const CalculatePriceForm = () => {
   }, [width, height, dikte]);
 
   return (
-    <div className="flex justify-between p-1 mt-5 relative">
+    <div className="flex justify-between p-4 mt-5 relative bg-slate-100 lg:bg-white">
       <div>
-        <h1 className="text-4xl font-bold text-center absolute z-10 bg-white my-6 p-4 pl-0">
+        <h1 className="text-2xl lg:text-4xl font-bold text-center lg:absolute z-10 lg:bg-white lg:my-6 p-4 pl-0">
           Voer afmetingen in en Bereken de prijs van uw glas
         </h1>
-        <div className="flex mt-24 gap-32">
-          <div className="flex flex-col">
+        <div className="lg:mt-28 lg:pl-6 flex flex-col gap-4">
+          <div className="flex gap-8 lg:gap-16 justify-center">
             <div className="flex flex-col justify-center mb-3">
-              <label className="text-2xl font-bold">Breedte</label>
+              <label className="text-md lg:text-2xl font-bold text-center">
+                Breedte
+              </label>
               <input
                 type="number"
-                className="border-2 border-gray-300 p-2 rounded-lg w-44"
-                placeholder="Breedte"
+                className="border-2 border-gray-300 p-2 rounded-lg w-20 lg:w-44"
+                placeholder="mm"
                 value={width}
                 onChange={(e) => setWidth(e.target.value as unknown as number)}
               />
             </div>
             <div className="flex flex-col justify-center mb-3">
-              <label className="text-2xl font-bold">Hoogte</label>
+              <label className="text-md lg:text-2xl font-bold text-center">
+                Hoogte
+              </label>
               <input
                 type="number"
-                className="border-2 border-gray-300 p-2 rounded-lg w-44"
-                placeholder="Hoogte"
+                className="border-2 border-gray-300 p-2 rounded-lg w-20 lg:w-44"
+                placeholder="mm"
                 value={height}
                 onChange={(e) => setHeight(e.target.value as unknown as number)}
               />
             </div>
             <div className="flex flex-col justify-center mb-3">
-              <label className="text-2xl font-bold">Dikte</label>
+              <label className="text-md lg:text-2xl font-bold text-center">
+                Dikte
+              </label>
               <input
                 type="number"
-                className="border-2 border-gray-300 p-2 rounded-lg w-44"
-                placeholder="Dikte"
+                className="border-2 border-gray-300 p-2 rounded-lg w-20 lg:w-44"
+                placeholder="mm"
                 value={dikte}
                 onChange={(e) => setDikte(e.target.value as unknown as number)}
               />
             </div>
           </div>
-          <div className="flex flex-col justify-center align-center">
-            <div className="flex flex-col gap-2">
-              <h1 className="text-2xl font-bold text-center tracking-wider">
+          <div className="flex lg:flex-col gap-4 lg:gap-2 justify-center items-center">
+            <div className="flex lg:flex-col gap-1 items-center justify-center">
+              <h1 className="text-xl lg:text-2xl font-bold text-center tracking-wider">
                 PRIJS: {!prijs && `€ ...`}
               </h1>
-              <h2 className="text-2xl font-bold text-center text-orange-500">
+              <h2 className="text-xl lg:text-2xl font-bold text-center text-orange-500">
                 {prijs && `€ ${prijs.toFixed(2)}`}
               </h2>
             </div>
             {prijs && (
-              <button className="bg-orange-500 text-white hover:scale-105 duration-200  px-5 py-2 text-xl rounded-lg mt-10">
-                Bestellen Nu
+              <button className="bg-orange-500 text-white hover:scale-105 duration-200  px-2 lg:px-5 py-2 text-md lg:text-xl rounded-lg  w-40 lg:m-auto ">
+                Bestellen nu
               </button>
             )}
           </div>
         </div>
       </div>
       <Image
-        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+        className="hidden lg:block relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
         src="/glass.png"
         alt="Next.js Logo"
         width={550}
--- components/static-header.tsx
+++ components/static-header.tsx
@@ -1,66 +1,65 @@
 "use client";
 import Image from "next/image";
-import { useScrollPosition } from "../hooks/useScrollPosition";
 
 const StaticHeader = () => {
-  const scrollPosition = useScrollPosition();
-
   return (
     <>
-      {scrollPosition === 0 && (
-        <div className="flex justify-between ">
-          <div className="flex items-center justify-center gap-6">
-            <Image
-              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-              src="/beste-prijs.png"
-              alt="Next.js Logo"
-              width={45}
-              height={45}
-              priority
-            />
+      <div className="hidden lg:flex justify-between ">
+        <div className="flex items-center justify-center gap-6">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src="/beste-prijs.png"
+            alt="Next.js Logo"
+            width={30}
+            height={30}
+            priority
+          />
 
-            <p className="text-sm uppercase font-bold">BESTE PRIJS</p>
-          </div>
-          <div className="flex items-center justify-center gap-6">
-            <Image
-              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert "
-              src="/superior-coating.png"
-              alt="Next.js Logo"
-              width={45}
-              height={45}
-              priority
-            />
+          <p className="text-xs uppercase font-bold tracking-wider">
+            BESTE PRIJS
+          </p>
+        </div>
+        <div className="flex items-center justify-center gap-6">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert "
+            src="/superior-coating.png"
+            alt="Next.js Logo"
+            width={30}
+            height={30}
+            priority
+          />
 
-            <p className="text-sm font-bold">SUPERIOR COATING</p>
-          </div>
-          <div className="flex items-center justify-center gap-6">
-            <Image
-              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert "
-              src="/snelle-levering.svg"
-              alt="Next.js Logo"
-              width={45}
-              height={45}
-              priority
-            />
+          <p className="text-xs font-bold tracking-wider">SUPERIOR COATING</p>
+        </div>
+        <div className="flex items-center justify-center gap-6">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert "
+            src="/snelle-levering.svg"
+            alt="Next.js Logo"
+            width={40}
+            height={40}
+            priority
+          />
 
-            <p className="text-sm font-bold">SNELLE LEVERING</p>
-          </div>
-          <div className="flex items-center justify-center  gap-6">
-            <Image
-              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert "
-              src="/365-dagen-bereikbaar.png"
-              alt="Next.js Logo"
-              width={45}
-              height={45}
-              priority
-            />
-            <div>
-              <p className="text-sm font-bold">365 DAGEN PER JAAR</p>
-              <p className="text-sm font-bold">OPEN & BEREIKBAAR</p>
-            </div>
+          <p className="text-xs font-bold tracking-wider">SNELLE LEVERING</p>
+        </div>
+        <div className="flex items-center justify-center  gap-6">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert "
+            src="/365-dagen-bereikbaar.png"
+            alt="Next.js Logo"
+            width={30}
+            height={30}
+            priority
+          />
+          <div>
+            <p className="text-xs font-bold tracking-wider">
+              365 DAGEN PER JAAR
+            </p>
+            <p className="text-xs font-bold tracking-wide">OPEN & BEREIKBAAR</p>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
